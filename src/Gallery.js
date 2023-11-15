44bddccbeec2760c3c7e118f58b263331c1c5b77
ztd5049 <ztd5049@psu.edu>
2023-11-15 15:56:49 -0500

    First commit

---
+++ src/Gallery.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Gallery() {
+  return (
+    <div class="main__container">
+      <div class="PreviewCards">
+        <h1>Gallery</h1>
+        <div class="PreviewCards__container">
+
+            <div class="PreviewCards__card"></div>
+
+
+            <div class="PreviewCards__card"></div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/imgImports.js
@@ -0,0 +1,15 @@
+import _BridgeStraight from './images/BridgeStraight.jpg'
+import _DriversLicenseCover from './images/DriversLicenseCover.jpg'
+import _SaturnV from './images/SaturnV.png'
+
+
+const data = [
+{img: _BridgeStraight,
+ name: "BridgeStraight", folder: "./images"},
+{img: _DriversLicenseCover,
+ name: "DriversLicenseCover", folder: "./images"},
+{img: _SaturnV,
+ name: "SaturnV", folder: "./images"},
+
+]
+export default data;
